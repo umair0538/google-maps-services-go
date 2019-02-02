e21bf0d84daa486adeac9d1dddd41a70bb4051a7
Umair Shahid <umair.shahid@easytaxi.sa>
2019-02-02 23:50:43 +0500

    Only get basic fields for place details API

--- places.go
+++ places.go
@@ -440,6 +440,8 @@ func (r *PlaceDetailsRequest) params() url.Values {
 		q.Set("language", r.Language)
 	}
 
+	q.Set("fields", "address_component,adr_address,alt_id,formatted_address,geometry,icon,id,name,permanently_closed,photo,place_id,plus_code,scope,type,url,utc_offset,vicinity")
+
 	return q
 }
 
